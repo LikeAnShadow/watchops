dd5f4a1a61b6f63488a29bea7a374514c3d6ed61
WhiteMoon030 <loris.els@hotmail.de>
2025-03-13 19:27:03 +0100

    more restructuring

---
+++ frontend/src/app/_movie/movie.tsx
@@ -0,0 +1,8 @@
+import styles from './movie.module.css'
+
+export default function Movie(){
+    return (
+        <div className={styles.movie_container}>
+        </div>
+    );
+}
--- frontend/src/app/movie.tsx
+++
@@ -1,8 +0,0 @@
-import style from './movie.module.css'
-
-export default function Movie(){
-    return (
-        <div className={style.movie_container}>
-        </div>
-    );
-}
