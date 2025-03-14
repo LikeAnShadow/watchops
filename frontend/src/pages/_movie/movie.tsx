44c880a65ef6250e4cdf04491d008ed4a72a2c5c
WhiteMoon030 <loris.els@hotmail.de>
2025-03-14 12:22:12 +0100

    added movie input elements

--- frontend/src/pages/_movie/movie.tsx
+++ frontend/src/pages/_movie/movie.tsx
@@ -1,8 +1,16 @@
 import styles from './movie.module.css'
+import Form from 'next/form'
 
-export default function Movie(){
+export default function Movie({ data }){
     return (
-        <div className={styles.movie_container}>
-        </div>
+        <Form action="" className={styles.movie_container}>
+            <label className={styles.movie_label}>Titel</label>
+            <input className={styles.movie_input} type='text'></input>
+            <label className={styles.movie_label}>Jahr</label>
+            <input className={styles.movie_input}type='date'></input>
+            <label className={styles.movie_label}>Beschreibung</label>
+            <textarea className={styles.movie_textarea} ></textarea>
+            <button className={styles.movie_button} type='submit'>Speichern</button>
+        </Form>
     );
 }
